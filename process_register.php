fa6a6352653c3ac0ec02b8c89117517c71fe7f75
Aishwarya1129 <56844052+Aishwarya1129@users.noreply.github.com>
2019-10-22 00:43:47 +0530

    Update process_register.php

--- process_register.php
+++ process_register.php
@@ -15,7 +15,7 @@
 			$msg.="<li>Please Enter your Password Again.....";
 		}
 		
-		if(!ereg("^[a-z0-9_]+[a-z0-9_.]*@[a-z0-9_-]+[a-z0-9_.-]*\.[a-z]{2,5}$",$_POST['mail']))
+		if(!preg("^[a-z0-9_]+[a-z0-9_.]*@[a-z0-9_-]+[a-z0-9_.-]*\.[a-z]{2,5}$",$_POST['mail']))
 		{
 			$msg.="<li>Please Enter Your Valid Email Address...";
 		}
